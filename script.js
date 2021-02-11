6be6973ef2d13dbdf7b8b1e89d6e8e19a6f169a0
Jessica <simply.me05@hotmail.com>
2021-02-10 22:34:44 -0500

    corrected js code line 26 through line 31

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Write password to the #password input
+function writePassword() {
+
+    
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  }
+  
+
+function generatePassword(){}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -12,7 +24,7 @@ var upperCaseChars = ["A","B","C","D", "E","F","G","H","I","J","K","L","M","N","
 // structure the numeric chars as an array
 var numbers = ["0,1,2,3,4,5,6,7,8,9"]; 
 var specChar = ["$%&#$@*!_^+-/"];
-var combinedChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0,1,2,3,4,5,6,7,8,9","$%&#$@*!_^+-/];
+var combinedChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0,1,2,3,4,5,6,7,8,9","$%&#$@*!_^+-"]
 
 // convert the returned value of prompt to a number 
 
@@ -21,7 +33,6 @@ var passwordLength = prompt("How many characters would you like your password to
 // check if passwordLength is lt 8 or gt 128, if it's not valid, you can alert the user "your password should ..."
 // if statement to check if passwordLength is valid
 
-// for (var i = 0; i < 8;) { //NEED TO FIX LOOP
 if (passwordLength <8){
   alert("Password must be a minimun of 8 characters")
   prompt("How many characters would you like your password to contain?")
@@ -30,7 +41,6 @@ if (passwordLength >128){
   alert("Password max length is 128 characters")
   prompt("How many characters would you like your password to contain?")
 };
-// };
 
 // create variables for uppercase, numeric, specChars
 var hasLowerCase = confirm ("Do you want to include lower case characters?");
@@ -53,26 +63,19 @@ if (hasLowerCase === true ) {
   specChar+=combinedChars;
 };
 
+// here you will end up with an array that contains all of the relevant characters
 var randomPassword =[];
 
-// if statement for uppercase, same as lower case
-// if statement for numbers, same as lower case
-// if statement for specChars, same as lower case
-// here you will end up with an array that contains all of the relevant characters
 
 // pulling one random character out, do it passwordLenght times (with a loop)
+for (var i =0; i<=passwordLength; i++){
 var randomPassword = Math.floor(Math.random() * combinedChars.length);
-console.log(randomPassword, combinedChars[randomPassword]); // 5, "A"
+// console.log(randomPassword, combinedChars[randomPassword]); // 5, "A"
+}
 
-// Write password to the #password input
-function writePassword() {
+randomPassword+=lowerCaseChars;
+randomPassword+=upperCaseChars;
+randomPassword+=numbers;
+randomPassword+=specChar;
 
-    
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  }
-  
 
-function generatePassword(){}
